51d467ef9ec3c9816cf8b9df82b907ae7d740a6d
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-03-19 18:22:56 -0400

    devMenu pages added to new package

--- src/main/java/com/groupfour/patientportal_vgui/DevMenuController.java
+++ src/main/java/com/groupfour/patientportal_vgui/DevMenuController.java
@@ -4,23 +4,77 @@
  */
 package com.groupfour.patientportal_vgui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+//import javafx.collections.ObservableList;
+//import javafx.scene.control.ListView;
+//import javafx.scene.control.TextField;
+//import javafx.stage.FileChooser;
 
-/**
- * FXML Controller class
- *
- * @author Alexander
- */
-public class DevMenuController implements Initializable {
+public class DevMenuController implements Initializable 
+{
 
+    @FXML
+    private Button buttonViewPatient;
+    @FXML
+    private Button buttonViewDoctor;
+    @FXML
+    private Button buttonUpdatePatient;
+    @FXML
+    private Button buttonUpdateDoctor;
+    @FXML
+    private Button buttonDeletePatient;
+    @FXML
+    private Button buttonQuitApp;
+    
+    @FXML
+    private void handleButtonViewPatient() throws IOException
+    {
+        switchToSecondary();
+    }
+    @FXML
+    private void handlebuttonViewDoctor()
+    {
+        
+    }
+    @FXML
+    private void handlebuttonUpdatePatient()
+    {
+        
+    }
+    @FXML
+    private void handlebuttonUpdateDoctor()
+    {
+        
+    }
+    @FXML
+    private void handlebuttonDeletePatient()
+    {
+        
+    }
+    @FXML
+    private void handlebuttonQuitApp()
+    {
+        System.exit(0);
+    }
+    
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+    
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
+    public void initialize(URL url, ResourceBundle rb) 
+    {
         // TODO
     }    
-    
 }
