64505ae4a786b2fbc633fd55f9f9c553dafe1609
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-03-19 16:28:05 -0400

    let there be light

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+/* doesn't work with source level 1.8:
+module com.groupfour.patientportal_vgui {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.groupfour.patientportal_vgui to javafx.fxml;
+    exports com.groupfour.patientportal_vgui;
+}
+*/
