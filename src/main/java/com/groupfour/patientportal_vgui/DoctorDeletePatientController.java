7d9b74091e30fa23aabb5031af2725bd42ad3852
Cipher0k <101017063+Cipher0k@users.noreply.github.com>
2022-03-30 21:27:59 -0400

    delete controller

---
+++ src/main/java/com/groupfour/patientportal_vgui/DoctorDeletePatientController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.groupfour.patientportal_vgui;
+import java.net.URL;
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Yasin
+ */
+public class DoctorDeletePatientController implements Initializable {
+    
+     @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }   
+    
+}
