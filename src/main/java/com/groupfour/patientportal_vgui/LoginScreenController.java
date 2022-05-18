6fc8ac581f4321472f6b5f88885f15b169d1d8e1
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-05-18 13:19:00 -0400

    login fix

--- src/main/java/com/groupfour/patientportal_vgui/LoginScreenController.java
+++ src/main/java/com/groupfour/patientportal_vgui/LoginScreenController.java
@@ -115,79 +115,89 @@ public class LoginScreenController implements Initializable {
     private void Login (ActionEvent event) throws Exception {
         String userName = text_user.getText();
         String passWord = text_pass.getText();
-        try 
+        
+        if ("".equals(userName) && "".equals(passWord))
         {
-            con = DatabaseConnection.connectDB();
-            Statement st = (Statement) con.createStatement();
-            rs = st.executeQuery( "Select * FROM LOGIN WHERE Username = '" + userName + "' AND Password = '" + passWord + "';");
-            
-            con2 = DatabaseConnection.connectDB();
-            Statement st2 = (Statement) con2.createStatement();
-            rs2 = st2.executeQuery( "Select * FROM LOGIN WHERE Email = '" + userName + "' AND Password = '" + passWord + "';");
-            
-            if (rs.next() && rs.getString(2).equals(userName) && rs.getString(3).equals(passWord)) 
+            JOptionPane.showMessageDialog( null, "Please fill in all fields" , "Error",JOptionPane.ERROR_MESSAGE );
+        }
+        else
+        {
+            try 
             {
-                App.currentUser = new CurrentUser(rs.getString(1), rs.getString(5));
-                
-                if (rs.getString(5).toUpperCase().equals("PATIENT"))
-                {           
-                        App.setRoot("patientDashboard");
-                        //JOptionPane.showMessageDialog(null, "Your login was successful.");
-                        //button_login.getScene().getWindow().hide();
-                        //Parent root = FXMLLoader.load(getClass().getResource("patientDashboard.fxml")); 
-                        //Stage mainStage = new Stage();
-                        //Scene scene = new Scene(root);
-                        //mainStage.setScene(scene);
-                        //mainStage.show();
-                }
-                else if (rs.getString(5).toUpperCase().equals("DOCTOR")) 
+                con = DatabaseConnection.connectDB();
+                Statement st = (Statement) con.createStatement();
+                rs = st.executeQuery( "Select * FROM LOGIN WHERE Username = '" + userName + "' AND Password = '" + passWord + "';");
+
+                con2 = DatabaseConnection.connectDB();
+                Statement st2 = (Statement) con2.createStatement();
+                rs2 = st2.executeQuery( "Select * FROM LOGIN WHERE Email = '" + userName + "' AND Password = '" + passWord + "';");
+
+                if (rs.next() && rs.getString(2).equals(userName) && rs.getString(3).equals(passWord)) 
                 {
-                        App.setRoot("doctorDashboard");
+                    App.currentUser = new CurrentUser(rs.getString(1), rs.getString(5));
+
+                    if (rs.getString(5).toUpperCase().equals("PATIENT"))
+                    {           
+                            App.setRoot("patientDashboard");
+                            //JOptionPane.showMessageDialog(null, "Your login was successful.");
+                            //button_login.getScene().getWindow().hide();
+                            //Parent root = FXMLLoader.load(getClass().getResource("patientDashboard.fxml")); 
+                            //Stage mainStage = new Stage();
+                            //Scene scene = new Scene(root);
+                            //mainStage.setScene(scene);
+                            //mainStage.show();
+                    }
+                    else if (rs.getString(5).toUpperCase().equals("DOCTOR")) 
+                    {
+                            App.setRoot("doctorDashboard");
+                    }
+                    else if (rs.getString(5).toUpperCase().equals("LAB")) 
+                    {
+                            App.setRoot("servicesDashboard");
+                    }
+                    else if (rs.getString(5).toUpperCase().equals("PHARMACY")) 
+                    {
+                            App.setRoot("servicesDashboard");
+                    }
+                    else if (rs.getString(5).toUpperCase().equals("OFFICE")) 
+                    {
+                            App.setRoot("servicesDashboard");
+                    }
                 }
-                else if (rs.getString(5).toUpperCase().equals("LAB")) 
+                else if (rs2.next() && rs2.getString(4).equals(userName) && rs2.getString(3).equals(passWord)) 
                 {
-                        App.setRoot("servicesDashboard");
+                    App.currentUser = new CurrentUser(rs2.getString(1), rs.getString(5));
+
+                    if (rs2.getString(5).toUpperCase().equals("PATIENT"))
+                    {       
+                            App.setRoot("patientDashboard");   
+                    }
+                    else if (rs2.getString(5).toUpperCase().equals("DOCTOR")) 
+                    {
+                            App.setRoot("doctorDashboard");  
+                    }
+                    else if (rs2.getString(5).toUpperCase().equals("LAB")) 
+                    {
+                            App.setRoot("servicesDashboard"); 
+                    }
+                    else if (rs2.getString(5).toUpperCase().equals("PHARMACY")) 
+                    {
+                            App.setRoot("servicesDashboard"); 
+                    }
+                    else if (rs2.getString(5).toUpperCase().equals("OFFICE")) 
+                    {
+                            App.setRoot("servicesDashboard"); 
+                    }
                 }
-                else if (rs.getString(5).toUpperCase().equals("PHARMACY")) 
-                {
-                        App.setRoot("servicesDashboard");
+                else
+                { 
+                 JOptionPane.showMessageDialog( null, "Incorrect username or password." , "Error", 
+                 JOptionPane.ERROR_MESSAGE );     
                 }
-                else if (rs.getString(5).toUpperCase().equals("OFFICE")) 
-                {
-                        App.setRoot("servicesDashboard");
-                }
-            }
-            else if (rs2.next() && rs2.getString(4).equals(userName) && rs2.getString(3).equals(passWord)) 
-            {
-                App.currentUser = new CurrentUser(rs2.getString(1), rs.getString(5));
-                
-                if (rs2.getString(5).toUpperCase().equals("PATIENT"))
-                {       
-                        App.setRoot("patientDashboard");   
-                }
-                else if (rs2.getString(5).toUpperCase().equals("DOCTOR")) 
-                {
-                        App.setRoot("doctorDashboard");  
-                }
-                else if (rs2.getString(5).toUpperCase().equals("LAB")) 
-                {
-                        App.setRoot("servicesDashboard"); 
-                }
-                else if (rs2.getString(5).toUpperCase().equals("PHARMACY")) 
-                {
-                        App.setRoot("servicesDashboard"); 
-                }
-                else if (rs2.getString(5).toUpperCase().equals("OFFICE")) 
-                {
-                        App.setRoot("servicesDashboard"); 
-                }
-            }
-            else
-            { 
-             JOptionPane.showMessageDialog( null, "Incorrect username or password." , "Error", 
-             JOptionPane.ERROR_MESSAGE );     
-            }
-        } catch (Exception e) {JOptionPane.showMessageDialog( null, "Please fill in all fields" , "Error",JOptionPane.ERROR_MESSAGE );}
+            } catch (Exception e) {JOptionPane.showMessageDialog( null, "Please fill in all fields" , "Error",JOptionPane.ERROR_MESSAGE );}
+        }
+        
+        
     }
      
     
